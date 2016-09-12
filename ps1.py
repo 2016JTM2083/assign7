553f52c195c4401c115c63fdd8c0e3a3919ac361
2016JTM2083 <poojatayal97@gmail.com>
2016-09-12 13:52:26 +0530

    frequency of each word in input string

--- ps1.py
+++ ps1.py
@@ -1,3 +1,13 @@
 ###### this is the first .py file ###########
+import sys
+wordstring=str(sys.argv)
+wordlist = wordstring.split()
+wordfreq = []
+for w in wordlist:
+    wordfreq.append(wordlist.count(w))
 
+print("String\n" + wordstring +"\n")
+print("List\n" + str(wordlist) + "\n")
+print("Frequencies\n" + str(wordfreq) + "\n")
+print("Pairs\n" + str(zip(wordlist, wordfreq)))
 ####### write your code here ##########
