c847e49c3f63b08a8592787a94d1ffffa65863f3
2016JTM2083 <poojatayal97@gmail.com>
2016-09-12 16:17:59 +0530

    Problem Statement3

--- ps3.py
+++ ps3.py
@@ -1,3 +1,68 @@
 ###### this is the third .py file ###########
 
-####### write your code here ##########
+print "1. Add new record"
+print "2. Delete record"
+print "3. modify record"
+print "4. show record "
+print "5. Exit "
+dict={}
+d=dict
+
+while true:
+  print "enter value of choice"
+  choice=int(input())
+
+	if (choice==1):
+		print "enter name"
+		name=str(input());
+		d.setdefault(name, [])
+		print "City: ",
+		city=input();
+		print "District:",
+		dist=input()
+		print "State : ",
+		state=input();
+		d[name].append(city)
+		d[name].append(dist)
+		d[name].append(state)
+		
+        if (choice==2):
+		name=input();
+		for name in d:
+			del d[name]
+                continue
+
+	if (choice==3):
+		name=input().strip();
+		for name in d:
+			del d[name]
+		d.setdefault(name, [])
+		print "City: ",
+		city=input();
+		print "District:",
+		dist=input()
+		print "State : ",
+		state=input();
+		d[name].append(city)
+		d[name].append(dist)
+		d[name].append(state)
+		continue
+	if (choice==4):
+		name=input();
+		print d[name]
+		continue
+		
+	if (choice==5):
+		break
+
+print "enter name"
+name=input();
+b=list();
+for name in d:
+	l=d[name]
+	for var in l:
+		var=var[0:3]
+		b.append(var.upper())		
+		
+	print "H_CC_NO = " , b
+
