af3cab2a95a8ff038e57d73f2570779388fbdd2b
2016JTM2083 <poojatayal97@gmail.com>
2016-09-12 14:52:22 +0530

    Code updated

--- ps1.py
+++ ps1.py
@@ -1,13 +1,22 @@
-###### this is the first .py file ###########
+###### this is the first .py file ###########import sys
 import sys
-wordstring=str(sys.argv)
-wordlist = wordstring.split()
-wordfreq = []
-for w in wordlist:
-    wordfreq.append(wordlist.count(w))
+import collections
+print sys.argv[1:]
+dict = {}
+for word in sys.argv[1:]:
+	if word not in dict.keys():
+  		dict[word] = 1
+ 	else:
+  		dict[word] += 1
 
-print("String\n" + wordstring +"\n")
-print("List\n" + str(wordlist) + "\n")
-print("Frequencies\n" + str(wordfreq) + "\n")
-print("Pairs\n" + str(zip(wordlist, wordfreq)))
+print dict
+
+# Function to find top 3 words
+def top3words(dict):
+	counts = collections.Counter(dict)
+	return [elem for elem, _ in counts.most_common(3)]
+#function calling
+top = top3words(dict)
+print top
+
 ####### write your code here ##########
