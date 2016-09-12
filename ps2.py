8ca55bc8d12affa025f0e28ef06d626c4d7d7a34
2016JTM2083 <poojatayal97@gmail.com>
2016-09-12 15:22:36 +0530

    Problem Statement 2

--- ps2.py
+++ ps2.py
@@ -1,3 +1,19 @@
 ###### this is the second .py file ###########
 
-####### write your code here ##########
+
+import random,math						      #import packages for using random and maths functions
+						       
+no_users=input("Enter number of users \n") 		
+count=0
+
+								        #For loop to generate location of each user randomly
+for i in range(no_users): 							 
+	(x,y)=(random.random()*2- 1, random.random()*2-1)              
+	dist=math.sqrt(x*x+y*y)    					# Find the distance of each user from origin
+	if dist<=1:   							#Condition to check user lie within unit radius or not
+		count+=1 
+
+print "No. of users in unit radius:",count                 						
+percentage=(float(count)/float(no_users))*100						
+print "percentage of users in unit radius:",percentage
+
